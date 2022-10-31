2e207db3db79825e6c566896cc89bb47f9d56656
Victor Vilar <victor.svilar@gmail.com>
2022-10-31 19:21:57 -0300

    adicionada configuração inicial.

---
+++ src/main/java/minicursoapiheroku/MiniCursoApiHerokuApplication.java
@@ -0,0 +1,13 @@
+package minicursoapiheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCursoApiHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCursoApiHerokuApplication.class, args);
+	}
+
+}
